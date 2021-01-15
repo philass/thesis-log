ba152de61a2acb0faff2daa3fcf7a23b0fbb236a
Philip Lassen <philiplassen+git@gmail.com>
2021-01-15 03:49:19 -0800

    handle multiple return values

--- codegen/codeGen.hs
+++ codegen/codeGen.hs
@@ -1,13 +1,37 @@
+main = do
+  let jse = JSEntryPoint { name = "entry1", parameters = ["i32"], ret = ["u64", "u8"] }
+  putStr (javascriptWrapper [jse])
+
 type EntryPointName = String
 type EntryPointType = String
 data JSEntryPoint = JSEntryPoint { name :: String,
                                    parameters :: [EntryPointType],
                                    ret :: [EntryPointType]
                                  }
+initFunc :: String
+initFunc = 
+  unlines
+  [" initData(data) {",
+  "    var nDataByes = data.BYTES_PER_ELEMENT",
+  "    var dataptr = Module._malloc(nDataBytes);",
+  "    var dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);",
+  "    dataHeap.set(new Uint8Array(data.buffer));",
+  "    return dataHeap",
+  "}"]
 
+initDataHeap :: Int -> String -> String
+initDataHeap idx arrType = "    var dataHeap" ++ show idx ++ " = initData(new " ++ arrType ++ "(1));"
+
+
+resDataHeap :: Int -> String -> String
+resDataHeap idx arrType = 
+  "    var res" ++ show idx ++ " = new " ++ arrType ++ "dataHeap" ++ show idx ++ ".buffer," ++
+  " dataHeap" ++ show idx ++ ".byteOffset, 1);"
+                            
 javascriptWrapper :: [JSEntryPoint] -> String
 javascriptWrapper entryPoints = unlines 
   [cwraps,
+  initFunc,
   classDef,
   constructor,
   (unlines $ map jsWrapEntryPoint entryPoints),
@@ -18,7 +42,7 @@ cwraps :: String
 cwraps = 
   unlines
   ["futhark_context_config_new = Module.cwrap(",
-   "'futhark_context_config_new', 'number', []",
+   "  'futhark_context_config_new', 'number', []",
    ");",
    "",
    "futhark_context_new = Module.cwrap(",
@@ -48,28 +72,27 @@ constructor =
 jsWrapEntryPoint :: JSEntryPoint -> String
 jsWrapEntryPoint jse =
   unlines
-  [func_name ++ "(" ++ args ++ ") {",
+  ["  " ++ func_name ++ "(" ++ args ++ ") {",
   cwrapEntryPoint func_name,
-  "var data = new " ++ convType ++ "(1);",
-  "var nDataByes = data.BYTES_PER_ELEMENT",
-  "var dataptr = Module._malloc(nDataBytes);",
-  "var dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);",
-  "dataHeap.set(new Uint8Array(data.buffer));",
-  "futhark_" ++ func_name ++ "(this.ctx, dataHeap.byteOffset, " ++ args ++ ");",
-  "var result = new " ++ convType ++ "(dataHeap.buffer, dataHeap.byteOffset, 1);",
-  "futhark_context_sync(this.ctx);",
-  "return result[0];"]
+  inits,
+  "    futhark_" ++ func_name ++ "(this.ctx, " ++ rets ++ ", " ++ args ++ ");",
+  results,  
+  "    futhark_context_sync(this.ctx);",
+  "    return result[0];"]
   where
     func_name = name jse
-    convType = typeConversion (head (ret jse))
+    convTypes = map typeConversion $ ret jse
+    inits = unlines $ map (\i -> initDataHeap i (convTypes !! i)) [0..(length (ret jse)) - 1]
+    results = unlines $ map (\i -> resDataHeap i (convTypes !! i)) [0..(length (ret jse)) - 1]
+    rets = tail (unwords [",dataHeap" ++ show i ++ ".byteOffset" | i <- [1..length (parameters jse)]])
     args = tail (unwords [",in" ++ show i | i <- [1..length (parameters jse)]])
     
 cwrapEntryPoint :: EntryPointName -> String
 cwrapEntryPoint ename = 
   unlines 
-  ["futhark_" ++ ename ++ " = Module.cwrap(", 
-   "  'futhark_" ++ ename ++ "', 'number', ['number', 'number', 'number']",
-   ");"]
+  ["    futhark_" ++ ename ++ " = Module.cwrap(", 
+   "      'futhark_" ++ ename ++ "', 'number', ['number', 'number', 'number']",
+   "    );"]
 
 
 typeConversion :: String -> String
